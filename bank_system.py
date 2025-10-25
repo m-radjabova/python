c9e450726bd46b5075a4018a469fcc06fe4f852f
m-radjabova <muslimarajabova1997@gmail.com>
2025-10-25 10:32:11 +0500

    bank_system updated

---
+++ bank_system.py
@@ -0,0 +1,23 @@
+from bank_accaunt import BankAccount
+
+
+class BankSystem:
+    def __init__(self):
+        self.accounts = []
+    
+    def add_account(self,bank_account:BankAccount):
+        self.accounts.append(bank_account)
+    
+    def find_account_by_pincode(self,pincode):
+        for account in  self.accounts:
+            if account.get_pincode() == pincode:
+                return account
+        return False
+    
+    def find_account_by_account_number(self,account_number):
+        for account in  self.accounts:
+            if account.get_account_number() == account_number:
+                return account
+        return False
+
+
